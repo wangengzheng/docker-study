1b19138cb718d0fdff0e11fc5b5aea925b164887
zhengwangeng <1021349117@qq.com>
2019-03-20 11:27:39 +0800

    add ssserver-service

---
+++ ssserver-server/readertemplate.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+#export  ssservername=$1 \
+       	ssserverpassword=$2 \
+	ssserverport=$3\
+
+#	eval "echo \"$(cat $4)\"" > $4
+
+#copy docker-compose.template.yml $4
+
+
+sed 's/${ssservername}/'"$1"'/g; s/${ssserverpassword}/'"$2"'/g; s/${ssserverport}/'"$3"'/g;' docker-compose.template.yml > $4
+
+
